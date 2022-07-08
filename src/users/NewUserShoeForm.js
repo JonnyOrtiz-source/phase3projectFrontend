f8567c23c20fd687ad17280da7edd16391dd260a
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-07-07 22:04:21 -0400

    added dropdown to select a shoe for a user

--- src/users/NewUserShoeForm.js
+++ src/users/NewUserShoeForm.js
@@ -1,16 +1,39 @@
 import { useState } from 'react';
-// import Dropdown from './Dropdown';
+
+const Dropdown = ({ label, value, options, handleChange }) => {
+   return (
+      <label>
+         {label}
+         <select className="text-black" value={value} onChange={handleChange}>
+            {options.map((option) => (
+               <option value={option.value}>{option.label}</option>
+            ))}
+         </select>
+      </label>
+   );
+};
 
 function NewUserShoeForm({
    user,
    toggleShowNewUserShoeForm,
    handleAddUserShoe,
+   allShoes,
 }) {
    const [shoe_type, setShoeType] = useState('');
    const [purchase_date, setPurchaseDate] = useState('');
    const [color, setColor] = useState('');
    const [size, setSize] = useState('');
    const [UPC, setUPC] = useState('');
+
+   const options = allShoes.map((shoe) => {
+      return { label: shoe.shoe_name, value: shoe.id };
+   });
+
+   const [shoe_id, setShoe_Id] = useState(options[0].shoe_name);
+
+   const handleChange = (e) => {
+      setShoe_Id(e.target.value);
+   };
 
    const handleSubmit = async (e) => {
       e.preventDefault();
@@ -21,19 +44,21 @@ function NewUserShoeForm({
          size,
          UPC,
          user_id: user.id,
+         shoe_id,
       };
       handleAddUserShoe(formData);
    };
 
    return (
-      <div>
+      <div className="font-bold">
          <form onSubmit={handleSubmit}>
-            {/* <Dropdown
-               label="What do we eat?"
+            <Dropdown
+               label="Select shoe: "
                options={options}
-               value={value}
-               onChange={handleChange}
-            />{' '} */}
+               value={shoe_id}
+               handleChange={handleChange}
+            />{' '}
+            <br />
             <label htmlFor="shoeType">Shoe Type: &nbsp;</label>
             <input
                className="text-black"
@@ -41,7 +66,8 @@ function NewUserShoeForm({
                name="shoeType"
                type="text"
                onChange={(e) => setShoeType(e.target.value)}
-            />
+            />{' '}
+            &nbsp; &nbsp;
             <label htmlFor="purchaseDate">Purchase Date: &nbsp;</label>
             <input
                className="text-black"
@@ -50,6 +76,7 @@ function NewUserShoeForm({
                type="date"
                onChange={(e) => setPurchaseDate(e.target.value)}
             />
+            <br />
             <label htmlFor="color">Color: &nbsp;</label>
             <input
                className="text-black"
@@ -57,7 +84,8 @@ function NewUserShoeForm({
                name="color"
                type="text"
                onChange={(e) => setColor(e.target.value)}
-            />
+            />{' '}
+            &nbsp; &nbsp;
             <label htmlFor="size">Size: &nbsp;</label>
             <input
                className="text-black"
@@ -65,7 +93,8 @@ function NewUserShoeForm({
                name="size"
                type="text"
                onChange={(e) => setSize(e.target.value)}
-            />
+            />{' '}
+            &nbsp; &nbsp;
             <label htmlFor="UPC">UPC: &nbsp;</label>
             <input
                className="text-black"
@@ -74,9 +103,20 @@ function NewUserShoeForm({
                type="text"
                onChange={(e) => setUPC(e.target.value)}
             />
-            <button onClick={toggleShowNewUserShoeForm}>Cancel</button>{' '}
+            <br />
+            <button
+               onClick={toggleShowNewUserShoeForm}
+               className="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded"
+            >
+               Cancel
+            </button>{' '}
             &nbsp;&nbsp;
-            <button type="submit">Add Shoe</button>
+            <button
+               type="submit"
+               className="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded"
+            >
+               Add Shoe
+            </button>
          </form>
       </div>
    );
--- src/users/UserDetail.js
+++ src/users/UserDetail.js
@@ -1,11 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import NewUserShoeForm from './NewUserShoeForm';
 
 function UserDetail({ user = {}, addUserShoe, deleteUserShoe, deleteUser }) {
    const { id, first_name, last_name, shoes } = user;
-   console.log(shoes);
    const [showNewUserShoeForm, setShowNewUserShoeForm] = useState(false);
+
+   const BASE_URL = 'http://localhost:9292';
+   const [allShoes, setAllShoes] = useState([]);
+
+   useEffect(() => {
+      fetch(`${BASE_URL}/shoes`, {
+         method: 'GET',
+         headers: {
+            'Content-type': 'application/json',
+            Accept: 'application/json',
+         },
+      })
+         .then((r) => r.json())
+         .then((shoes) => setAllShoes(shoes))
+         .catch((err) => {
+            alert(err);
+         });
+   }, []);
 
    const toggleShowNewUserShoeForm = () => {
       setShowNewUserShoeForm(!showNewUserShoeForm);
@@ -19,37 +36,40 @@ function UserDetail({ user = {}, addUserShoe, deleteUserShoe, deleteUser }) {
    return (
       <div>
          <div>
-            <h1>
+            <div className="text-2xl font-bold">
                {first_name} {last_name}
-            </h1>
+            </div>
             <Link to={`/users/{id}/edit`}>✍🏼 </Link>
             <button onClick={() => deleteUser(id)}>❌</button>
          </div>
          <div>
-            <h1>
+            <div className="text-2xl">
                Shoes{' '}
                {!showNewUserShoeForm && (
                   <button onClick={toggleShowNewUserShoeForm}>
                      ➕ Add Shoe
                   </button>
                )}
-            </h1>
+            </div>
          </div>
          <ul>
             {shoes?.map((userShoe) => (
                <li key={userShoe.id}>
                   <Link to={`/shoes/${userShoe.shoe_id}`}>
-                     Shoe Type: {userShoe.user_shoes[0].shoe_type} &nbsp; Brand:{' '}
-                     {userShoe.brand} &nbsp; Shoe Name: {userShoe.shoe_name}{' '}
-                     &nbsp; Sex: {userShoe.sex} &nbsp; Purchase Date:{' '}
-                     {userShoe.user_shoes[0].purchase_date}&nbsp; Color:{' '}
-                     {userShoe.user_shoes[0].color} &nbsp; Size:{' '}
+                     Shoe Type: {userShoe.user_shoes[0].shoe_type} {'  '}
+                     Brand: {userShoe.brand} {'  '} Shoe Name:{' '}
+                     {userShoe.shoe_name}
+                     {'  '}
+                     <br /> Sex: {userShoe.sex} Purchase Date: {'  '}
+                     {userShoe.user_shoes[0].purchase_date} {'  '} Color:{'  '}
+                     {userShoe.user_shoes[0].color} {'  '} Size:{'  '}
                      {userShoe.user_shoes[0].size}
                   </Link>{' '}
                   &nbsp;&nbsp;
                   <button onClick={() => deleteUserShoe(id, userShoe.id)}>
                      ❌
                   </button>
+                  <hr />
                </li>
             ))}
             {showNewUserShoeForm && (
@@ -58,6 +78,7 @@ function UserDetail({ user = {}, addUserShoe, deleteUserShoe, deleteUser }) {
                      user={user}
                      toggleShowNewUserShoeForm={toggleShowNewUserShoeForm}
                      handleAddUserShoe={handleAddUserShoeSubmit}
+                     allShoes={allShoes}
                   />
                </li>
             )}
